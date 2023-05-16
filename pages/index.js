3531b998e014110510706e7d3261baad06784e49
Adrian Hoche <adrian@hoche.at>
2023-05-16 02:30:11 +0200

    Updated description

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm <strong>codingCoala</strong>. I've studied business informatics and I'm now working as a product owner.</p>
+        <p>Hello, I'm <strong>codingCoala</strong>. I've studied business informatics and I'm now working as a product owner. Test.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
